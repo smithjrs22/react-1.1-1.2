e54f8f263faaa08bcf92a02ce161e3ff073a104c
9CD1KC2\admin <smithjrs22@gmail.com>
2022-04-14 22:01:43 -0400

    one submit

---
+++ src/component/content.jsx
@@ -0,0 +1,11 @@
+let Content = (prop) => {
+    return (
+        <div>
+            <p>{prop.part1}{prop.exercise1}</p>
+            <p>{prop.part2}{prop.exercise2}</p>
+            <p>{prop.part3}{prop.exercise3}</p>
+        </div>
+    );
+};
+
+export default Content 
---
+++ src/component/header.jsx
@@ -0,0 +1,9 @@
+let Header = (prop) => {
+    return (
+        <>
+            <h1>{prop.course}</h1>
+        </>
+    );
+};
+
+export default Header
